48bd1a832ce7b3e18a3047cc074671b2a1e32fbe
David Besse <dahodprod@gmail.com>
2024-12-22 20:34:58 +0100

    'fix:calendar: import chevronprops'

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { DayPicker } from "react-day-picker"
+import { ChevronProps, DayPicker } from "react-day-picker"
 
 import { cn } from "@/lib/utils"
 import { buttonVariants } from "@/components/ui/button"
